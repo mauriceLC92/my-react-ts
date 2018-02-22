506c72563bbeae424f9cf10f694c21f5d5e27997
Maurice Le Cordier <maurice.lecordier@SAMAC141.local>
2018-02-22 13:55:44 +0200

    attempted to convert

---
+++ src/components/Dashboard/GenerateClearButtons.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+
+interface State {
+
+}
+
+interface Props {
+    onGen: boolean
+    onClear: boolean
+}
+export class GenerateClearButtons extends React.Component <Props, State>{
+
+    renderHorseList = () => {
+        // this.setState({horseListEmpty: true});
+        this.props.onGen(true)
+      }
+    
+    clearHorseList = () => {
+        // this.setState({horseListEmpty: false});
+        this.props.onClear(false)
+    }
+
+    render(){
+        return(
+            <div className="ui medium buttons">
+                <button className="positive ui button" onClick={this.renderHorseList}>Generate Horses</button>
+                    <div className="or"></div>
+                 <button className="ui button" onClick={this.clearHorseList} >Clear Horses</button>
+            </div>
+        );
+    }
+}
---
+++ src/components/Dashboard/StartAndStopButtons.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+
+interface State {
+
+}
+
+interface Props {
+    onStart: boolean
+    onStop: boolean
+}
+export class StartAndStopButtons extends React.Component <Props, State>{
+    constructor(props: Props){
+        super(props);
+
+        this.startRace = this.startRace.bind(this);
+        this.resetRace = this.resetRace.bind(this);
+    }
+
+    startRace = () =>{
+        // this.setState({run: true})
+        this.props.onStart(true)
+    }
+
+    resetRace = () =>{
+        // this.setState({run: true})
+        this.props.onStop(false)
+    }
+    
+    render(){
+        return(
+            <div className="ui medium buttons">
+                  <button id="startButton" className="positive ui button" onClick={this.startRace}>Render Race Tracks</button>
+                  <div className="or"></div>
+                  <button className="negative ui button" onClick={this.resetRace}>Clear Race Tracks</button>
+            </div>
+        );
+    }
+}
